1e37881e99569fd415d78f0b2591a68f63232812
mohitshekhawat123 <kakashihatake320s@gmail.com>
2026-01-04 20:28:11 +0530

    Fix: Correct import paths case sensitivity for Linux deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react"
-import Sidebar from "./components/sidebar"
-import Player from "./components/player"
+import Sidebar from "./components/Sidebar"
+import Player from "./components/Player"
 import Display from "./components/Display"
 import { PlayerContext } from "./context/PlayerContext"
 
